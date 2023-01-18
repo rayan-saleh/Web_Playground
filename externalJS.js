6fb9967bba6412467f80ccb0ec9229304e486fe6
rayan-saleh <86924567+rayan-saleh@users.noreply.github.com>
2023-01-18 22:18:06 +0000

    initial commit

---
+++ externalJS.js
@@ -0,0 +1,16 @@
+// let message;
+
+// if (login == 'Employee') {
+//   message = 'Hello';
+// } else if (login == 'Director') {
+//   message = 'Greetings';
+// } else if (login == '') {
+//   message = 'No login';
+// } else {
+//   message = '';
+// }
+
+
+let message = (login == 'Employee') ?  message = 'Hello' : (login == 'Director') ?  message = 'Greetings' : (login == '') ? 
+message = 'No login' : message = ''; 
+
---
+++ objects_play.js
@@ -0,0 +1,9 @@
+"use strict";
+
+
+
+let userName = "hi there";
+
+let userNameUpper = userName.toUpperCase();
+console.log(userNameUpper)
+
